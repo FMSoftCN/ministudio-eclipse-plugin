8870c267a16d779eb04de3a5df44cd346ac7eb32
xwyan <xwyan@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2008-10-29 08:19:37 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@10 fea7bd08-1b65-41d0-8d0d-2588895579b9

---
+++ org.minigui.eclipse.cdt.mstudio/src/org/minigui/eclipse/cdt/mstudio/wizards/NewMiniGUIUIFileWizardPage.java
@@ -0,0 +1,185 @@
+package org.minigui.eclipse.cdt.mstudio.wizards;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
+
+public class NewMiniGUIUIFileWizardPage extends WizardPage {
+    private Text containerText;
+    private Text fileText;
+
+    private ISelection selection;
+	
+	protected NewMiniGUIUIFileWizardPage(ISelection selection) {
+		super("MiniGUIUIWizardPage");
+		
+		setTitle("Create a MiniGUI UI File");
+		this.selection = selection;
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		// TODO Auto-generated method stub
+        Composite container = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        container.setLayout(layout);
+        layout.numColumns = 3;
+        layout.verticalSpacing = 9;
+        Label label = new Label(container, SWT.NULL);
+        label.setText("&Source Folder:");
+
+        containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+        containerText.setLayoutData(gd);
+        containerText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
+
+        Button button = new Button(container, SWT.PUSH);
+        button.setText("Browse...");
+        button.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                handleBrowse();
+            }
+        });
+        
+        label = new Label(container, SWT.NULL);
+        label.setText("&File name:");
+
+        fileText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        gd = new GridData(GridData.FILL_HORIZONTAL);
+        fileText.setLayoutData(gd);
+        fileText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
+
+        initialize();
+        dialogChanged();
+        setControl(container);       
+	}
+    /**
+     * Tests if the current workbench selection is a suitable container to use.
+     */
+
+    private void initialize() {
+        if (selection != null && selection.isEmpty() == false
+                && selection instanceof IStructuredSelection) {
+            IStructuredSelection ssel = (IStructuredSelection) selection;
+            if (ssel.size() > 1)
+                return;
+            Object obj = ssel.getFirstElement();
+            if (!(obj instanceof IResource) && (obj instanceof IAdaptable))
+                obj = ((IAdaptable)obj).getAdapter(IResource.class);
+            if (obj instanceof IResource) {
+                IContainer container;
+                if (obj instanceof IContainer)
+                    container = (IContainer) obj;
+                else
+                    container = ((IResource) obj).getParent();
+                containerText.setText(container.getFullPath().makeRelative().toString());
+            }
+        }
+        fileText.setFocus();
+    }
+	/**
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
+	 */
+
+	private void handleBrowse() {
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select source folder");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				containerText.setText(((Path) result[0]).makeRelative().toString());
+			}
+		}
+	}
+
+	/**
+	 * Ensures that both text fields are set.
+	 */
+
+	private void dialogChanged() {
+		IResource container = ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(new Path(getContainerName()));
+		String fileName = getFileName();
+
+		if (getContainerName().length() == 0) {
+			updateStatus("Source folder must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+			updateStatus("Source folder must exist");
+			return;
+		}
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
+			return;
+		}
+		if (fileName.length() == 0) {
+			updateStatus("File name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("File name must be valid");
+			return;
+		}
+		
+		final IFile file = ((IContainer)container).getFile(new Path(fileName));
+		updateStatus(file.toString());
+		if (file.exists()) {
+			updateStatus("File already exists");
+			return;
+		}
+		
+		String ext = "";
+		int dotLoc = fileName.lastIndexOf('.');
+		if (dotLoc != -1) {
+			ext = fileName.substring(dotLoc + 1);
+		}
+		if (ext.equalsIgnoreCase("ui") == false) {
+			updateStatus("File extension must be \"ui\"");
+			return;
+		}
+		updateStatus(null);
+	}
+
+	private void updateStatus(String message) {
+		setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+	public String getContainerName() {
+		return containerText.getText();
+	}
+
+	public String getFileName() {
+		return fileText.getText();
+	}
+}
