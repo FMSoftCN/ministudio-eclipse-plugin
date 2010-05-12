73af2f1d8256f46da90f3d5fb9718a1835fe0a3f
xwyan <xwyan@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-05-12 05:55:09 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2007 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.minigui.eclipse.cdt.mstudio/src/org/minigui/eclipse/cdt/mstudio/editor/MrcEditorLauncher.java
+++ org.minigui.eclipse.cdt.mstudio/src/org/minigui/eclipse/cdt/mstudio/editor/MrcEditorLauncher.java
@@ -22,80 +22,83 @@ import org.minigui.eclipse.cdt.mstudio.project.MgProject;
 
 public class MrcEditorLauncher implements IEditorLauncher {
 
-	public MrcEditorLauncher() { }
-	
-	public void open(IPath file) 
-	{
-		CommandLauncher launcher = new CommandLauncher();
-		launcher.showCommand(true);
-		
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IFile ifile = root.getFileForLocation(file); 
-		IProject project = ifile.getProject();	
-		String binPath = new MgProject(project).getMStudioBinPath();
+    public MrcEditorLauncher() { }
+
+    public void open(IPath file) 
+    {
+        CommandLauncher launcher = new CommandLauncher();
+        launcher.showCommand(true);
+
+        IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+        IFile ifile = root.getFileForLocation(file); 
+        IProject project = ifile.getProject();	
+        String binPath = System.getenv("GUIBUILDER_PATH");
+        if (binPath == null) {
+            binPath = new MgProject(project).getMStudioBinPath();
+        }
         StringBuffer cmd = new StringBuffer("guibuilder");
-      
+
         if (System.getProperty("os.name").toLowerCase().indexOf("window") >= 0) {
             cmd.append(".exe");
         }
-       
-		Path editCommand = new Path (binPath+ File.separatorChar +cmd.toString());
-		
-		List<String> args = new ArrayList<String>();	
-		
-		IPath projectDir = removeFileName(file).removeLastSegments(1);
-		args.add("-project");
-		args.add(projectDir.toOSString());
-		args.add("-project-name");
-		args.add(projectDir.lastSegment());
-		
-		//add port information
-		MStudioSocketServerThread serverThread = MStudioSocketServerThread.getInstance();
-		if (serverThread != null) {
-			args.add("-addr");
-			args.add(serverThread.getAddress());
-			args.add("-port");
-			args.add(Integer.toString(serverThread.getPort()));
-			//System.out.println(args);
-		}
-		
-		IPath workingDir = projectDir;
-		Properties envProps = EnvironmentReader.getEnvVars();
-		envProps.setProperty("CWD", workingDir.toOSString());
-		envProps.setProperty("PWD", workingDir.toOSString());
-	
-		Process p = launcher.execute(editCommand, (String[])args.toArray(new String[args.size()]),
-				createEnvStringList(envProps), workingDir);
 
-		if (p != null) {
-			if (serverThread != null)
-				serverThread.addBuilderProcs(projectDir.lastSegment(), p);
-			//TODO, monitor this process ...
-		} else {
-			//TODO for error ...
-		}
-	}
+        Path editCommand = new Path (binPath+ File.separatorChar +cmd.toString());
 
-	private static IPath removeFileName(IPath path) {
-		if (path == null)
-			return null;
-		if (path.hasTrailingSeparator())
-			return path;
-		return path.removeLastSegments(1);
-	}
-	
-	private static String[] createEnvStringList(Properties envProps)
-	{
-		String[] env = null;
-		List<String> envList = new ArrayList<String>();
-		Enumeration<?> names = envProps.propertyNames();
-		if (names != null) {
-			while (names.hasMoreElements()) {
-				String key = (String) names.nextElement();
-				envList.add(key + "=" + envProps.getProperty(key));
-			}
-			env = (String[]) envList.toArray(new String[envList.size()]);
-		}
-		return env;
-	}
+        List<String> args = new ArrayList<String>();	
+
+        IPath projectDir = removeFileName(file).removeLastSegments(1);
+        args.add("-project");
+        args.add(projectDir.toOSString());
+        args.add("-project-name");
+        args.add(projectDir.lastSegment());
+
+        //add port information
+        MStudioSocketServerThread serverThread = MStudioSocketServerThread.getInstance();
+        if (serverThread != null) {
+            args.add("-addr");
+            args.add(serverThread.getAddress());
+            args.add("-port");
+            args.add(Integer.toString(serverThread.getPort()));
+            //System.out.println(args);
+        }
+
+        IPath workingDir = projectDir;
+        Properties envProps = EnvironmentReader.getEnvVars();
+        envProps.setProperty("CWD", workingDir.toOSString());
+        envProps.setProperty("PWD", workingDir.toOSString());
+
+        Process p = launcher.execute(editCommand, (String[])args.toArray(new String[args.size()]),
+                createEnvStringList(envProps), workingDir);
+
+        if (p != null) {
+            if (serverThread != null)
+                serverThread.addBuilderProcs(projectDir.lastSegment(), p);
+            //TODO, monitor this process ...
+        } else {
+            //TODO for error ...
+        }
+    }
+
+    private static IPath removeFileName(IPath path) {
+        if (path == null)
+            return null;
+        if (path.hasTrailingSeparator())
+            return path;
+        return path.removeLastSegments(1);
+    }
+
+    private static String[] createEnvStringList(Properties envProps)
+    {
+        String[] env = null;
+        List<String> envList = new ArrayList<String>();
+        Enumeration<?> names = envProps.propertyNames();
+        if (names != null) {
+            while (names.hasMoreElements()) {
+                String key = (String) names.nextElement();
+                envList.add(key + "=" + envProps.getProperty(key));
+            }
+            env = (String[]) envList.toArray(new String[envList.size()]);
+        }
+        return env;
+    }
 }
