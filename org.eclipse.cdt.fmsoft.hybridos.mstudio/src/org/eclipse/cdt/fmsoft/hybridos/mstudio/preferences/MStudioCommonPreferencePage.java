6717ade0ca7ea3df5fdc75e652946a43cb44cb2f
wangjian <wangjian@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-11 03:57:15 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2645 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/preferences/MStudioCommonPreferencePage.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/preferences/MStudioCommonPreferencePage.java
@@ -1,39 +1,59 @@
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.preferences;
 
+import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioPlugin;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class MStudioCommonPreferencePage extends PreferencePage implements
-		IWorkbenchPreferencePage {
+public class MStudioCommonPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+	
+	private String hybridosIntroduction = "HybridOS is an operating system dedicated to embedded device. " +
+	"It is based on Linux kernel, stable open source software (e.g. Gtk+, SDL, SQLite, and so on), " +
+	"and FMSoft's mature embedded software technologies, like embedded windowing system (MiniGUI), " +
+	"embedded web browser (mDolphin), and embedded J2SE solution."; 
 
 	public MStudioCommonPreferencePage() {
-		// TODO Auto-generated constructor stub
+		noDefaultAndApplyButton();
 	}
 
 	public MStudioCommonPreferencePage(String title) {
 		super(title);
-		// TODO Auto-generated constructor stub
+		noDefaultAndApplyButton();
 	}
 
 	public MStudioCommonPreferencePage(String title, ImageDescriptor image) {
 		super(title, image);
-		// TODO Auto-generated constructor stub
+		noDefaultAndApplyButton();
 	}
-
-	@Override
-	protected Control createContents(Composite parent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+	
 	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
 
+	}
+	
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		composite.setLayout(new GridLayout());
+		
+		Label imgLabel = new Label(composite, SWT.NONE);
+		if (null != imgLabel) {
+			MStudioPlugin.getDefault();
+			Image img = MStudioPlugin.getImageDescriptor("icons/hybridos-logo.png").createImage();
+			imgLabel.setImage(img);			
+		}
+		
+		Label intro = new Label(composite, SWT.WRAP);
+		if (null != intro){
+			intro.setFont(parent.getFont());
+         intro.setText(hybridosIntroduction);
+		}
+		return composite;
 	}
 
 }
