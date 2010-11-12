f6f3c987833166f2c10ad4425d8f1b2bfcfbfcb9
jxzhang <jxzhang@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-12 02:30:08 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2650 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/properties/MStudioDeployPropertyPage.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/properties/MStudioDeployPropertyPage.java
@@ -1,10 +1,12 @@
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.properties;
 
+
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbenchPropertyPage;
@@ -12,37 +14,38 @@ import org.eclipse.ui.dialogs.PropertyPage;
 
 public class MStudioDeployPropertyPage extends PropertyPage implements
 		IWorkbenchPropertyPage {
-
+	
+	private static String deployButtonText="Deploy this project to rootfs";
+	private static String deployButtonToolTipText="Deploy this project to rootfs";
+	private Button deployToRootfs;
 	public MStudioDeployPropertyPage() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	protected Control createContents(Composite parent) {
-		// TODO Auto-generated method stub
 		Composite composite = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		composite.setLayout(layout);
+		composite.setLayout(new GridLayout());
 		
 		GridData data = new GridData(GridData.FILL);
 		data.grabExcessHorizontalSpace = true;
 		composite.setLayoutData(data);
-		//TODO: create controls
-		
+		//Init the button
+		deployToRootfs = new Button(composite,SWT.CHECK);
+		deployToRootfs.setText(deployButtonText);		
+		deployToRootfs.setToolTipText(deployButtonToolTipText);
 		loadPersistentSettings();
 		return composite;
 	}
 
 	private void loadPersistentSettings() {
-		MStudioProject mStudioProject = new MStudioProject((IProject)getElement());
-		boolean defDeployable = mStudioProject.getDefaultDeployable();
-		//TODO set control default selection type
+		MStudioProject mStudioProject = new MStudioProject((IProject)getElement());		
+		deployToRootfs.setSelection(mStudioProject.getDefaultDeployable());		
 	}
 	
 	private boolean savePersistentSettings() {
 		 MStudioProject mStudioProject = new MStudioProject((IProject)getElement());
-		 //TODO
-		 return mStudioProject.setDefaultDeployable(false);
+		 return mStudioProject.setDefaultDeployable(deployToRootfs.getSelection());
 	}
 	
 	public boolean performOk() {
