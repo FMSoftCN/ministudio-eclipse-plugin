e9ac14f98c71288e1be5e10f930310f58a65bd67
wangjian <wangjian@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-16 02:07:00 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2666 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/preferences/MStudioCommonPreferencePage.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/preferences/MStudioCommonPreferencePage.java
@@ -1,31 +1,17 @@
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.preferences;
 
-import java.util.Iterator;
-import java.util.Vector;
-
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioPlugin;
-import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.PlatformUI;
 
 public class MStudioCommonPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 	
@@ -55,6 +41,7 @@ public class MStudioCommonPreferencePage extends PreferencePage implements IWork
 	protected Control createContents(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NULL);
 		composite.setLayout(new GridLayout());
+		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
 		Label imgLabel = new Label(composite, SWT.NONE);
 		if (null != imgLabel) {
@@ -65,7 +52,7 @@ public class MStudioCommonPreferencePage extends PreferencePage implements IWork
 		
 		Label intro = new Label(composite, SWT.WRAP);
 		if (null != intro){
-			intro.setFont(parent.getFont());
+			//intro.setFont(parent.getFont());
          intro.setText(hybridosIntroduction);
 		}
 		return composite;
