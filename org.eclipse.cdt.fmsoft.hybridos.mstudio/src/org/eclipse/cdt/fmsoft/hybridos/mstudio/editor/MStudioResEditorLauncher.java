424b3562a1848c6ef1ef8de2321764c46537edce
wangxin <wangxin@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-23 09:30:35 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2742 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/editor/MStudioResEditorLauncher.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/editor/MStudioResEditorLauncher.java
@@ -1,3 +1,18 @@
+/*********************************************************************
+ * Copyright (C) 2005 - 2010, Beijing FMSoft Technology Co., Ltd.
+ * Room 902, Floor 9, Taixing, No.11, Huayuan East Road, Haidian
+ * District, Beijing, P. R. CHINA 100191.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Beijing FMSoft Technology Co., Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall
+ * use it only in accordance you entered into with FMSoft.
+ *
+ *			http://www.minigui.com
+ *
+ *********************************************************************/
+
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.editor;
 
 import java.io.File;
@@ -6,10 +21,6 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 
-import org.eclipse.cdt.core.CommandLauncher;
-import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioSocketServerThread;
-import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
-import org.eclipse.cdt.utils.spawner.EnvironmentReader;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -20,99 +31,120 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubMonitor;
+
 import org.eclipse.ui.IEditorLauncher;
+
+import org.eclipse.cdt.core.CommandLauncher;
+import org.eclipse.cdt.utils.spawner.EnvironmentReader;
+import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
+import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioSocketServerThread;
 
 
 public class MStudioResEditorLauncher implements IEditorLauncher {
 
-    public MStudioResEditorLauncher() { }
+	private final static String MSEL_GUIBUILDER_PATH = "GUIBUILDER_PATH";
+	private final static String MSEL_TASK_COLLECTING = "mStudio Runner - Collecting Data";
+	private final static String MSEL_TASK_STARTING   = "mStudio Runner - Starting GUIBuilder: ";
+	private final static String MSEL_EMPTY_STR = "";
 
-    public void open(IPath file) 
-    {
-    	IProgressMonitor monitor = new NullProgressMonitor();
-    	SubMonitor subMonitor = SubMonitor.convert(monitor, 2);
-    	subMonitor.newChild(1).subTask("mStudio Runner - Collecting Data");
-    	
-        CommandLauncher launcher = new CommandLauncher();
-        launcher.showCommand(true);
-        IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-        IFile ifile = root.getFileForLocation(file); 
-        IProject project = ifile.getProject();	
-        String binPath = new MStudioProject(project).getMStudioBinPath();
-        if (binPath == null || binPath.equals("")) {
-            binPath = System.getenv("GUIBUILDER_PATH");
-        }
+	public MStudioResEditorLauncher() {
+	}
 
-        StringBuffer cmd = new StringBuffer("guibuilder");
+	public void open(IPath file) {
 
-        if (System.getProperty("os.name").toLowerCase().indexOf("window") >= 0) {
-            cmd.append(".exe");
-        }
+		IProgressMonitor monitor = new NullProgressMonitor();
+		SubMonitor subMonitor = SubMonitor.convert(monitor, 2);
+		subMonitor.newChild(1).subTask(MSEL_TASK_COLLECTING);
 
-        Path editCommand;
-        if (binPath == null || binPath.equals("")) {
-        	editCommand = new Path (cmd.toString());
-        }
-        else
-        	editCommand = new Path (binPath+ File.separatorChar +cmd.toString());
-        
-        List<String> args = new ArrayList<String>();	
+		CommandLauncher launcher = new CommandLauncher();
+		launcher.showCommand(true);
 
-        IPath projectDir = removeFileName(file).removeLastSegments(1);
-        args.add("-project");
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IFile ifile = root.getFileForLocation(file);
+		IProject project = ifile.getProject();
+		String binPath = new MStudioProject(project).getMStudioBinPath();
 
-        args.add(projectDir.toOSString());
-        args.add("-project-name");
-        args.add(projectDir.lastSegment());
+		if (binPath == null || binPath.equals(MSEL_EMPTY_STR)) {
+			binPath = System.getenv(MSEL_GUIBUILDER_PATH);
+		}
 
-        //add port information
-        MStudioSocketServerThread serverThread = MStudioSocketServerThread.getInstance();
-        if (serverThread != null) {
-            args.add("-addr");
-            args.add(serverThread.getAddress());
-            args.add("-port");
-            args.add(Integer.toString(serverThread.getPort()));
-        }
+		StringBuffer cmd = new StringBuffer("guibuilder");
 
-        IPath workingDir = projectDir;
-        Properties envProps = EnvironmentReader.getEnvVars();
-        envProps.setProperty("CWD", workingDir.toOSString());
-        envProps.setProperty("PWD", workingDir.toOSString());
+		if (System.getProperty("os.name").toLowerCase().indexOf("window") >= 0) {
+			cmd.append(".exe");
+		}
 
-        try {
-            Process p = launcher.execute(editCommand, (String[])args.toArray(new String[args.size()]),
-                    createEnvStringList(envProps), workingDir, monitor);
-            if (p != null) {
-            	subMonitor.newChild(1).subTask("mStudio Runner - Starting GUIBuilder: " + launcher.getCommandLine());
-                if (serverThread != null)
-                    serverThread.addBuilderProcs(projectDir.lastSegment(), p);
-            } 
-        } catch (CoreException e) {
-        	System.out.println(e);
-        }
-        monitor.done();
-    }
+		Path editCommand = null;
 
-    private static IPath removeFileName(IPath path) {
-        if (path == null)
-            return null;
-        if (path.hasTrailingSeparator())
-            return path;
-        return path.removeLastSegments(1);
-    }
+		if (binPath == null || binPath.equals(MSEL_EMPTY_STR)) {
+			editCommand = new Path (cmd.toString());
+		} else {
+			editCommand = new Path (binPath+ File.separatorChar + cmd.toString());
+		}
 
-    private static String[] createEnvStringList(Properties envProps)
-    {
-        String[] env = null;
-        List<String> envList = new ArrayList<String>();
-        Enumeration<?> names = envProps.propertyNames();
-        if (names != null) {
-            while (names.hasMoreElements()) {
-                String key = (String) names.nextElement();
-                envList.add(key + "=" + envProps.getProperty(key));
-            }
-            env = (String[]) envList.toArray(new String[envList.size()]);
-        }
-        return env;
-    }
+		List<String> args = new ArrayList<String>();
+		IPath projectDir = removeFileName(file).removeLastSegments(1);
+
+		args.add("-project");
+		args.add(projectDir.toOSString());
+		args.add("-project-name");
+		args.add(projectDir.lastSegment());
+
+		//add port information
+		MStudioSocketServerThread serverThread = MStudioSocketServerThread.getInstance();
+		if (serverThread != null) {
+			args.add("-addr");
+			args.add(serverThread.getAddress());
+			args.add("-port");
+			args.add(Integer.toString(serverThread.getPort()));
+		}
+
+		IPath workingDir = projectDir;
+		Properties envProps = EnvironmentReader.getEnvVars();
+		envProps.setProperty("CWD", workingDir.toOSString());
+		envProps.setProperty("PWD", workingDir.toOSString());
+
+		try {
+			Process p = launcher.execute(editCommand, (String[])args.toArray(new String[args.size()]),
+					createEnvStringList(envProps), workingDir, monitor);
+			if (p != null) {
+				subMonitor.newChild(1).subTask(MSEL_TASK_STARTING + launcher.getCommandLine());
+				if (serverThread != null)
+					serverThread.addBuilderProcs(projectDir.lastSegment(), p);
+			}
+		} catch (CoreException e) {
+			System.out.println(e);
+		}
+
+		monitor.done();
+	}
+
+	private static IPath removeFileName(IPath path) {
+
+		if (path == null)
+			return null;
+
+		if (path.hasTrailingSeparator())
+			return path;
+
+		return path.removeLastSegments(1);
+	}
+
+	private static String[] createEnvStringList(Properties envProps) {
+
+		String[] env = null;
+		List<String> envList = new ArrayList<String>();
+		Enumeration<?> names = envProps.propertyNames();
+
+		if (names != null) {
+			while (names.hasMoreElements()) {
+				String key = (String) names.nextElement();
+				envList.add(key + "=" + envProps.getProperty(key));
+			}
+			env = (String[]) envList.toArray(new String[envList.size()]);
+		}
+
+		return env;
+	}
 }
+
