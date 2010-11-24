7879c93c417954c492ae2bd0decb9eaee8736051
jxzhang <jxzhang@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-24 10:19:57 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2773 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/wizards/MStudioDeployAutobootProjectsWizardPage.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/wizards/MStudioDeployAutobootProjectsWizardPage.java
@@ -1,28 +1,22 @@
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.wizards;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioEnvInfo;
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioMessages;
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioEnvInfo.MiniGUIRunMode;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
@@ -33,6 +27,7 @@ public class MStudioDeployAutobootProjectsWizardPage extends WizardPage {
 	private Button selectAll=null;
 	private Button upButton,downButton;
 	private IProject[] projects;
+	private ArrayList<String> projectOfChecked;
 	
 	public MStudioDeployAutobootProjectsWizardPage(String pageName) {
 		super(pageName);
@@ -49,40 +44,41 @@ public class MStudioDeployAutobootProjectsWizardPage extends WizardPage {
 		super(pageName, title, titleImage);
 		init();
 	}
-
-	@Override
+	
 	public void createControl(Composite parent) {
 		Composite topPanel;
 		topPanel = new Composite(parent, SWT.NONE);
 		topPanel.setLayout(new GridLayout());
 		topPanel.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
-		Composite com1=new Composite(topPanel,SWT.NONE);
+		Composite com1 = new Composite(topPanel,SWT.NONE);
 		com1.setLayout(new GridLayout());
 		com1.setLayoutData(new GridData());
-		Label title=new Label(com1,SWT.FILL);
+		Label title = new Label(com1,SWT.FILL);
 		title.setText("Auto start programes");
 		title.setLayoutData(new GridData(GridData.FILL_BOTH));
 		
-		Composite com2=new Composite(topPanel,SWT.NONE);		
+		Composite com2 = new Composite(topPanel,SWT.NONE);		
 		com2.setLayout(new GridLayout(2,false));
 		com2.setLayoutData(new GridData(GridData.FILL_BOTH));
-		GridData dg=new GridData(GridData.FILL_BOTH);
-		dg.verticalSpan=2;
+		GridData dg = new GridData(GridData.FILL_BOTH);
+		dg.verticalSpan = 2;
 		
-		table=new Table(com2, SWT.CHECK|SWT.V_SCROLL|SWT.H_SCROLL|SWT.FULL_SELECTION|SWT.FILL|SWT.MULTI|SWT.BORDER);
+		table=new Table(com2, SWT.CHECK | SWT.V_SCROLL | SWT.H_SCROLL |
+				SWT.FULL_SELECTION | SWT.FILL | SWT.MULTI | SWT.BORDER);
 		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
 		table.setLayout(new GridLayout());
 		table.setLayoutData(dg);
-		TableColumn col1=new TableColumn(table,SWT.NONE);
+		table.addListener(SWT.Selection,new TableListener());
+		TableColumn col1 = new TableColumn(table,SWT.NONE);
 		col1.setText("Start");
 		col1.setWidth(50);
-		TableColumn col2=new TableColumn(table,SWT.NONE); 	
+		TableColumn col2 = new TableColumn(table,SWT.NONE); 	
 		col2.setText("Program Name");
 		col2.setWidth(100);
 		
-		upButton=new Button(com2,SWT.NONE);
+		upButton = new Button(com2,SWT.NONE);
 		upButton.setLayoutData(new GridData(GridData.FILL));
 		upButton.setText("Move Up");
 		upButton.addSelectionListener(new SelectionListener(){
@@ -90,171 +86,164 @@ public class MStudioDeployAutobootProjectsWizardPage extends WizardPage {
 				
 			}
 			public void widgetSelected(SelectionEvent e) {
-				int index=table.getSelectionIndex();
+				int index = table.getSelectionIndex();
 				if(projects != null && index < projects.length){
-					IProject p=projects[index];
-					projects[index]=projects[index-1];
-					projects[index-1]=p;
-					table.setSelection(index-1);
+					keepCheckedItem();
+					IProject p = projects[index];
+					projects[index] = projects[index-1];
+					projects[index-1] = p;
 					updateTable();
+					table.setSelection(index-1);
 					updateButtons();
 				}				
-			}
+			}			
 			});
-		downButton=new Button(com2,SWT.NONE);
+		downButton = new Button(com2,SWT.NONE);
 		downButton.setLayoutData(new GridData(GridData.FILL));
 		downButton.setText("Move Down");
 		downButton.addSelectionListener(new SelectionListener(){
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// TODO Auto-generated method stub
-				
+			public void widgetDefaultSelected(SelectionEvent e) {	
 			}
-
-			@Override
 			public void widgetSelected(SelectionEvent e) {
-				int index=table.getSelectionIndex();
+				int index = table.getSelectionIndex();
 				if(projects != null && index < projects.length){
-					IProject p=projects[index];
-					projects[index]=projects[index+1];
-					projects[index-1]=p;
-					table.setSelection(index+1);
+					keepCheckedItem();
+					IProject p = projects[index];
+					projects[index] = projects[index+1];
+					projects[index+1] = p;
 					updateTable();
+					table.setSelection(index+1);					
 					updateButtons();
 				}
-			}
-			
+			}			
 		});
 				
-		selectAll=new Button(topPanel,SWT.CHECK);
+		selectAll = new Button(topPanel,SWT.CHECK);
 		selectAll.setText("Select All");
 		selectAll.addSelectionListener(new SelectionListener(){
 			public void widgetDefaultSelected(SelectionEvent e) {
 			}
 			public void widgetSelected(SelectionEvent e) {
-				if(selectAll.getSelection()){
-					table.selectAll();
+				TableItem[] items = table.getItems();
+				if(selectAll.getSelection()){					
+					for(int i=0; i<items.length; i++){
+						items[i].setChecked(true);
+					}
+					setPageComplete(true);
 				}
-				else{					
+				else{	
+					for(int i=0; i<items.length; i++){
+						items[i].setChecked(false);
+					}
+					setPageComplete(false);					
 				}
 			}});
 		
 		initAutoStartProgrames();		
 		setControl(topPanel);
-		setPageComplete(true);
-
+		setPageComplete(false);
 	}
 	public void update(){
-		table.clearAll();
+		table.setItemCount(0);
 		initAutoStartProgrames();
-		updateButtons();
-		MStudioDeployWizard.deployCanFinish=true;
 	}
-	private void updateButtons() {
+	//keep the state of the checked items
+	private void keepCheckedItem() {
+		if(projectOfChecked == null)
+			projectOfChecked = new ArrayList<String>();
+		projectOfChecked.clear();
+		TableItem[] itm = table.getItems();
+		for(int i=0; i<itm.length; i++){
+			if(itm[i].getChecked()){
+				projectOfChecked.add(itm[i].getText(1));
+			}
+		}
+	}
+	private void updateButtons() {		
 		if(MStudioEnvInfo.getInstance().getMgRunMode() == MiniGUIRunMode.thread.name()){
 			upButton.setEnabled(false);
 			downButton.setEnabled(false);
 			selectAll.setEnabled(false);
 		}
-		else{			
-			int index=table.getSelectionIndex();	
-			if(index < 0){
+		else{
+			if(projects.length <=0 || projects == null || table.getSelectionCount()<=0){
+				setPageComplete(false);
 				upButton.setEnabled(false);
 				downButton.setEnabled(false);
 				return;
 			}
-			if(index == 0){
+			int index = table.getSelectionIndex();	
+			if(index < 0){
+				upButton.setEnabled(false);
+				downButton.setEnabled(false);
+			}
+			else if(index == 0){
 				upButton.setEnabled(false);
 				downButton.setEnabled(true);
-				return;
 			}
-			if(index >0 && index < table.getItemCount()-1){
+			else if(index >0 && index < table.getItemCount()-1){
 				upButton.setEnabled(true);
 				downButton.setEnabled(true);
-				return;
 			}
-			if(index == table.getItemCount()-1){
+			else if(index == table.getItemCount()-1){
 				upButton.setEnabled(true);
+				downButton.setEnabled(false);
+			}
+			if(table.getItemCount() <= 1){
+				upButton.setEnabled(false);
 				downButton.setEnabled(false);
 				return;
 			}
 		}
 	}
 	private void updateTable(){
-		if(projects!=null){
-			for(int i=0;i<projects.length;i++){
-				TableItem item=new TableItem(table,SWT.NONE);
+		table.setItemCount(0);
+		if(projects != null){
+			for(int i=0; i<projects.length; i++){
+				TableItem item = new TableItem(table,SWT.CHECK);
 				item.setText(1,projects[i].getName());
+				if(projectOfChecked != null && projectOfChecked.contains(projects[i].getName()))
+					item.setChecked(true);
 			}
-		}
+		}		
 	}
 	//init the table data
 	private void initAutoStartProgrames(){
 		MStudioDeployWizard wizard = (MStudioDeployWizard) getWizard();
-		if(wizard==null)
+		if(wizard == null)
 			return;
-		projects=wizard.getDeployExeProjects();		
-		if(projects!=null){
-			for(int i=0;i<projects.length;i++){
-				TableItem item=new TableItem(table,SWT.NONE);
-			}
-		}		
+		projects = wizard.getDeployExeProjects();		
+		updateTable();	
 		updateButtons();
 	}
 	
 	public IProject[] getDeployAutobootProjects() {
+		if(projects == null || projects.length <=0)
+			return null;
 		return projects;
 	}
-	public class tableSelectChangedListener implements ISelectionChangedListener{
-		public void selectionChanged(SelectionChangedEvent event) {
-			table.getSelectionIndex();
+
+	public class TableListener implements Listener{
+		public void handleEvent(Event event) {
+			if(event.detail == SWT.CHECK){
+				keepCheckedItem();
+				if(projectOfChecked.size() == projects.length && projects != null){
+					selectAll.setSelection(true);
+				}
+				else{
+					selectAll.setSelection(false);
+				}
+				if(projectOfChecked.size() <=0 || projectOfChecked == null){
+					setPageComplete(false);					
+				}
+				else{
+					setPageComplete(true);
+				}
+				updateButtons();
+			}
+			else{
+				updateButtons();
+			}			
 		}		
-	}	
-	
-	public class TableViewerContentProvider implements IStructuredContentProvider{
-		public Object[] getElements(Object inputElement) {
-			if(inputElement instanceof List)
-				return ((List)inputElement).toArray();
-			return new Object[0];
-		}
-		public void dispose() {
-			
-		}
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-			//TODO
-		}
-		
-	}
-	public class TableViewerLabelProvider implements ITableLabelProvider{
-
-		@Override
-		public Image getColumnImage(Object element, int columnIndex) {
-			return null;
-		}
-
-		@Override
-		public String getColumnText(Object element, int columnIndex) {			
-			//if the element is class or orther object here would be ocurr an error	
-			return element.toString();
-		}
-
-		@Override
-		public void addListener(ILabelProviderListener listener) {
-		}
-
-		@Override
-		public void dispose() {
-		}
-
-		@Override
-		public boolean isLabelProperty(Object element, String property) {
-			return false;
-		}
-
-		@Override
-		public void removeListener(ILabelProviderListener listener) {
-		}
-		
 	}
 }
