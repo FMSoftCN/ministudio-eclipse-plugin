b88426aa90d4c5f478d289175ea80fe3bd296699
wangxin <wangxin@fea7bd08-1b65-41d0-8d0d-2588895579b9>
2010-11-24 03:10:30 +0000

    git-svn-id: svn+ssh://devsrv/home/projects/svn/mstudio/eclipse-plugin/trunk@2763 fea7bd08-1b65-41d0-8d0d-2588895579b9

--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/properties/MStudioDeployPropertyPage.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/properties/MStudioDeployPropertyPage.java
@@ -1,54 +1,77 @@
+/*********************************************************************
+ * Copyright (C) 2005 - 2010, Beijing FMSoft Technology Co., Ltd.
+ * Room 902, Floor 9, Taixing, No.11, Huayuan East Road, Haidian
+ * District, Beijing, P. R. CHINA 100191.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Beijing FMSoft Technology Co., Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall
+ * use it only in accordance you entered into with FMSoft.
+ *
+ *			http://www.minigui.com
+ *
+ *********************************************************************/
+
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.properties;
 
-
-import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+
 import org.eclipse.ui.IWorkbenchPropertyPage;
 import org.eclipse.ui.dialogs.PropertyPage;
 
-public class MStudioDeployPropertyPage extends PropertyPage implements
-		IWorkbenchPropertyPage {
-	
-	private static String deployButtonText="Deploy this project to rootfs";
-	private static String deployButtonToolTipText="Deploy this project to rootfs";
-	private Button deployToRootfs;
+import org.eclipse.core.resources.IProject;
+
+import org.eclipse.cdt.fmsoft.hybridos.mstudio.project.MStudioProject;
+
+
+public class MStudioDeployPropertyPage extends PropertyPage
+	implements IWorkbenchPropertyPage {
+
+	private final static String MSDPP_BUTTON_TEXT = "Deploy this project to rootfs";
+	private final static String MSDPP_BUTTON_TOOL_TIP_TEXT = "Deploy this project to rootfs";
+
+	private Button deployToRootfs = null;
+
 	public MStudioDeployPropertyPage() {
-		
 	}
 
 	@Override
 	protected Control createContents(Composite parent) {
+
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout());
-		
+
 		GridData data = new GridData(GridData.FILL);
 		composite.setLayoutData(data);
+
 		//Init the button
-		deployToRootfs = new Button(composite,SWT.CHECK);
-		deployToRootfs.setText(deployButtonText);		
-		deployToRootfs.setToolTipText(deployButtonToolTipText);
+		deployToRootfs = new Button(composite, SWT.CHECK);
+		deployToRootfs.setText(MSDPP_BUTTON_TEXT);
+		deployToRootfs.setToolTipText(MSDPP_BUTTON_TOOL_TIP_TEXT);
+
 		loadPersistentSettings();
+
 		return composite;
 	}
 
 	private void loadPersistentSettings() {
-		MStudioProject mStudioProject = new MStudioProject((IProject)getElement());		
-		deployToRootfs.setSelection(mStudioProject.getDefaultDeployable());		
+		MStudioProject mStudioProject = new MStudioProject((IProject)getElement());
+		deployToRootfs.setSelection(mStudioProject.getDefaultDeployable());
 	}
-	
+
 	private boolean savePersistentSettings() {
 		 MStudioProject mStudioProject = new MStudioProject((IProject)getElement());
 		 return mStudioProject.setDefaultDeployable(deployToRootfs.getSelection());
 	}
-	
+
 	public boolean performOk() {
 		return savePersistentSettings();
 	}
-
 }
+
--- org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/template/MStudioNewWizardTemplate.java
+++ org.eclipse.cdt.fmsoft.hybridos.mstudio/src/org/eclipse/cdt/fmsoft/hybridos/mstudio/template/MStudioNewWizardTemplate.java
@@ -1,8 +1,23 @@
+/*********************************************************************
+ * Copyright (C) 2005 - 2010, Beijing FMSoft Technology Co., Ltd.
+ * Room 902, Floor 9, Taixing, No.11, Huayuan East Road, Haidian
+ * District, Beijing, P. R. CHINA 100191.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Beijing FMSoft Technology Co., Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall
+ * use it only in accordance you entered into with FMSoft.
+ *
+ *			http://www.minigui.com
+ *
+ *********************************************************************/
 
 package org.eclipse.cdt.fmsoft.hybridos.mstudio.template;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
 import org.eclipse.cdt.managedbuilder.buildproperties.IBuildPropertyManager;
 import org.eclipse.cdt.managedbuilder.buildproperties.IBuildPropertyType;
 import org.eclipse.cdt.managedbuilder.buildproperties.IBuildPropertyValue;
@@ -10,50 +25,59 @@ import org.eclipse.cdt.managedbuilder.core.BuildListComparator;
 import org.eclipse.cdt.managedbuilder.core.IToolChain;
 import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.cdt.managedbuilder.ui.wizards.AbstractCWizard;
-import org.eclipse.cdt.ui.wizards.EntryDescriptor;
+
 import org.eclipse.jface.wizard.IWizard;
+
+import org.eclipse.cdt.ui.wizards.EntryDescriptor;
 import org.eclipse.cdt.ui.templateengine.Template;
 import org.eclipse.cdt.ui.templateengine.TemplateEngineUI;
+
 import org.eclipse.cdt.core.templateengine.TemplateInfo;
+
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.MStudioMessages;
 import org.eclipse.cdt.fmsoft.hybridos.mstudio.wizards.MStudioWizardHandler;
 
 
 public class MStudioNewWizardTemplate extends AbstractCWizard {
-	
-	private static final String[] MSTUDIO_TYPE = {"org.eclipse.cdt.fmsoft.hybridos.mstudio.buildArtefactType.exe",
-									"org.eclipse.cdt.fmsoft.hybridos.mstudio.buildArtefactType.sharedLib"};
+
+	private static final String[] MSTUDIO_TYPE =
+		{"org.eclipse.cdt.fmsoft.hybridos.mstudio.buildArtefactType.exe",
+		"org.eclipse.cdt.fmsoft.hybridos.mstudio.buildArtefactType.sharedLib"};
 
 	public static final String OTHERS_LABEL = MStudioMessages.getString("MStudioNewWizard.0");  
 	public static final String EMPTY_PROJECT = "Empty mStudio Project";
-	
+	public static final String MSNWT_GNU_EXE_DEBUG = "cdt.managedbuild.toolchain.gnu.exe.debug";
+	public static final String MSNWT_GNU_SO_DEBUG = "cdt.managedbuild.toolchain.gnu.so.debug";
+
 	public EntryDescriptor[] createItems(boolean supportedOnly, IWizard wizard) {
+
 		ArrayList<EntryDescriptor> items = new ArrayList<EntryDescriptor>();
 		IBuildPropertyManager bpm = ManagedBuildManager.getBuildPropertyManager();
-		
+
 		for (int size = 0; size < MSTUDIO_TYPE.length; size++) {
 			IBuildPropertyType bpt = bpm.getPropertyType(MSTUDIO_TYPE[size]);
-			
 			IBuildPropertyValue[] vs = bpt.getSupportedValues();
+
 			Arrays.sort(vs, BuildListComparator.getInstance());
-			
+
 			Template[] templates = TemplateEngineUI.getDefault().getTemplates();
-			
-			for (int bpvIdx=0; bpvIdx<vs.length; bpvIdx++) {
-				IToolChain[] tcs = ManagedBuildManager.getExtensionsToolChains(MStudioWizardHandler.ARTIFACT, vs[bpvIdx].getId(), false);
+
+			for (int bpvIdx = 0; bpvIdx < vs.length; bpvIdx++) {
+				IToolChain[] tcs = ManagedBuildManager.getExtensionsToolChains(
+										MStudioWizardHandler.ARTIFACT, vs[bpvIdx].getId(), false);
 				if (tcs == null || tcs.length == 0) continue;
-				
+
 				MStudioWizardHandler h = new MStudioWizardHandler(vs[bpvIdx], parent, wizard);
 				h.addTc(null);
 				for (int tcsIdx=0; tcsIdx<tcs.length; tcsIdx++) {
 					if (isValid(tcs[tcsIdx], supportedOnly, wizard)) {
 						String tcsId = tcs[tcsIdx].getId(); 
 
-						if (tcsId.equals("cdt.managedbuild.toolchain.gnu.exe.debug")
-								|| tcsId.equals("cdt.managedbuild.toolchain.gnu.so.debug")) {
+						if (tcsId.equals(MSNWT_GNU_EXE_DEBUG)
+								|| tcsId.equals(MSNWT_GNU_SO_DEBUG)) {
 							h.addTc(tcs[tcsIdx]);
 						}
-						
+
 					}
 				}
 
@@ -61,10 +85,10 @@ public class MStudioNewWizardTemplate extends AbstractCWizard {
 					// The project category item.
 					items.add(new EntryDescriptor(vs[bpvIdx].getId(), null, vs[bpvIdx].getName(), true, h, null));
 
-					for (int tmplIdx=0; tmplIdx < templates.length; tmplIdx++) {
+					for (int tmplIdx = 0; tmplIdx < templates.length; tmplIdx++) {
 						TemplateInfo templateInfo = templates[tmplIdx].getTemplateInfo();
-						if(templateInfo.getProjectType().equals(MSTUDIO_TYPE[size]))
-						{
+
+						if(templateInfo.getProjectType().equals(MSTUDIO_TYPE[size])) {
 							items.add(new EntryDescriptor(templates[tmplIdx].getTemplateId(),
 								//null,
 								vs[bpvIdx].getId(),
@@ -74,7 +98,8 @@ public class MStudioNewWizardTemplate extends AbstractCWizard {
 				}
 			}
 		}
-		
+
 		return (EntryDescriptor[])items.toArray(new EntryDescriptor[items.size()]);
 	}
 }
+
